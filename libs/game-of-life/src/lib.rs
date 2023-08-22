0686de50f98c1721d5495e4f247225665de4b753
Red Rapious <red.rapious@gmail.com>
2023-08-22 23:51:00 +0200

    Added a Board mechanism

--- libs/game-of-life/src/lib.rs
+++ libs/game-of-life/src/lib.rs
@@ -1,14 +1,67 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub struct Board {
+    grid: Vec<Vec<Cell>>,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+impl Board {
+    pub fn new(width: usize, height: usize) -> Self {
+        assert!(width > 0 && height > 0);
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        let grid = (0..height).map(|_| vec![Cell::Black; width]).collect();
+
+        Self { grid }
     }
+
+    pub fn grid(&self) -> &Vec<Vec<Cell>> {
+        &self.grid
+    }
+
+    fn width(&self) -> usize {
+        self.grid[0].len()
+    }
+
+    fn height(&self) -> usize {
+        self.grid.len()
+    }
+
+    fn cell_white_neighbours(&self, x: usize, y: usize) -> u8 {
+        assert!(x < self.width());
+        assert!(y < self.height());
+
+        let mut white_neighbours = 0;
+
+        for dx in -1..=1 {
+            for dy in [-1, 1] {
+                if 0 <= x as i32 + dx
+                    && x as i32 + dx < self.width() as i32
+                    && 0 <= y as i32 + dy
+                    && y as i32 + dy < self.height() as i32
+                    && self.grid[(y as i32 + dy) as usize][(x as i32 + dx) as usize] == Cell::White
+                {
+                    white_neighbours += 1;
+                }
+            }
+        }
+
+        white_neighbours
+    }
+
+    pub fn step(&mut self) {
+        self.grid = (0..self.width())
+            .map(|x| {
+                (0..self.height())
+                    .map(|y| match self.cell_white_neighbours(x, y) {
+                        3 => Cell::White,     // birth or stay alive
+                        2 => self.grid[y][x], // stay dead or stay alive
+                        _ => Cell::Black,     // no birth, or die of under/over-population
+                    })
+                    .collect()
+            })
+            .collect();
+    }
+}
+
+#[derive(Clone, Copy, PartialEq)]
+pub enum Cell {
+    Black,
+    White,
 }
